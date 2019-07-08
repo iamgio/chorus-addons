3f79fd2d52bb8d17c757193e30fd96ca6ee82e15
iAmGio <giogar2705@gmail.com>
2019-07-08 16:56:36 +0200

    Initial commit

---
+++ color-changer/color-changer.js
@@ -0,0 +1,47 @@
+load(chorus_js_api);
+
+credits = 'Chorus';
+
+function onLoad() {
+    translationMap = {
+        'color_changer': {
+            en: 'Color changer',
+            it: 'Cambio di colore',
+            de: 'Farbwechsler'
+        }
+    }
+}
+
+function onInit() {
+    var translation = translate('color_changer');
+
+    var edit = getMenuBarButton('edit');
+    edit.addButton(translation, function () {
+        var menu = new Menu(translation, true);
+        menu.layoutX = 300;
+        menu.layoutY = 300;
+        menu.filters = [];
+
+        var slider = new fxcontrols.Slider(.05, 1, 1);
+        menu.opacityProperty().bind(slider.valueProperty());
+        menu.children.add(slider);
+
+        var root = chorus.root
+
+        root.effect = new fx.effect.ColorAdjust();
+
+        listen(menu.layoutXProperty(), function () {
+            root.effect.hue = menu.layoutX / root.width;
+        })
+
+        listen(menu.layoutYProperty(), function () {
+            root.effect.saturation = menu.layoutY / root.height - .5
+        })
+
+        menu.onClose = function () {
+            root.effect = null;
+        }
+
+        menu.show();
+    })
+}
