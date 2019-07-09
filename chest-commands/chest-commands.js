ebd07e3dc42670e6a4e834177be5b63670c7a5af
iAmGio <giogar2705@gmail.com>
2019-07-09 17:24:14 +0200

    Chest-commands

---
+++ chest-commands/chest-commands.js
@@ -0,0 +1,34 @@
+load(chorus_js_api);
+
+credits = 'Chorus';
+
+function onInit() {
+    var format = new GUIFormat(
+        function getName(map) {
+            var settings = map.get('menu-settings');
+            return settings ? settings.get('name') : 'No title';
+        },
+        function getRows(map) {
+            var settings = map.get('menu-settings');
+            return settings ? settings.get('rows') : 1;
+        },
+        function getItems(map) {
+            var items = [];
+            map.keySet().forEach(function (key) {
+                if(key != 'menu-settings') {
+                    var itemSection = map.get(key);
+                    var item = itemSection.get('ID');
+                    var itemName = item ? item.contains(':') ? item.split(':')[0] : item : 'BEDROCK'
+                    var meta = item ? item.contains(':') ? item.split(':')[1] : 0 : 0
+                    var x = itemSection.get('POSITION-X');
+                    var y = itemSection.get('POSITION-Y');
+                    items.push(
+                        new GUIFormatItem(new GUIFormatPosition(x ? x : 0, y ? y : 0), itemName, meta)
+                    )
+                }
+            })
+            return items;
+        }
+    )
+    format.setActive();
+}
