db178cdff93f1b8c6899d2baaa43bdf08ba03f7b
Giorgio Garofalo <giogar2705@gmail.com>
2021-01-25 19:33:25 +0100

    chest-commands: add MATERIAL support

--- chest-commands/chest-commands.js
+++ chest-commands/chest-commands.js
@@ -1,7 +1,7 @@
 load(chorus_js_api);
 
 credits = 'Chorus';
-version = '1.0.1';
+version = '1.0.2';
 description = 'This add-on brings ChestCommand support to Chorus by loading GUIs from selection.'
 image = 'https://i.imgur.com/U3wp1qq.png'
 
@@ -21,6 +21,7 @@ function onInit() {
                 if (key != 'menu-settings') {
                     const itemSection = map.get(key);
                     let item = itemSection.get('ID');
+                    if(!item) item = itemSection.get('MATERIAL');
                     item = item ? item.toString().split(",")[0] : item.toString(); // Remove amounts (wool:3, 10 -> wool:3)
                     const itemName = item ? item.contains(':') ? item.split(':')[0] : item : 'BEDROCK'; // If item is null, use Bedrock. Otherwise, remove meta
                     const meta = item ? item.contains(':') ? item.split(':')[1] : 0 : 0; // Extract meta if exists, otherwhise 0
